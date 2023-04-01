73b1802db60e19675bbcfc2e2177835e165f96fd
Iva Petkova <iva1petkova@gmail.com>
2023-04-01 19:01:29 +0300

    create repo

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,24 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import '@/styles/Home.module.css';
+import React, { useState } from 'react';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  function changeCount(delta) {
+    setCount(count + delta);
+  }
+  return (
+    <>
+    <Button direction='up' handleClick={() => changeCount(1)}/>
+    <Button direction='down'handleClick={() => changeCount(-1)}/>
+    <div >Count: {count}</div>
+    </>
+  )
+}
+
+function Button({direction, handleClick}) {
+  return(
+    <button className='counter_button' onClick={handleClick}>{direction}</button>
+  )
 }
